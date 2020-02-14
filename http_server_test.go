d235a4c96c55487843076ccfb265d29ab961302f
ednailson <evbcjr@gmail.com>
2020-02-13 22:48:23 -0300

    Route test

--- http_server_test.go
+++ http_server_test.go
@@ -1,7 +1,10 @@
 package httping
 
 import (
+	"bytes"
 	. "github.com/onsi/gomega"
+	"net/http"
+	"strconv"
 	"testing"
 )
 
@@ -22,4 +25,25 @@ func TestNewRoute(t *testing.T) {
 	Expect(server.server).ShouldNot(BeNil())
 	route := server.NewRoute(nil, "/test")
 	Expect(route.route).ShouldNot(BeNil())
+}
+
+func TestNewRouteWithMethod(t *testing.T) {
+	RegisterTestingT(t)
+	server := NewHttpServer(port)
+	Expect(server.engine).ShouldNot(BeNil())
+	Expect(server.server).ShouldNot(BeNil())
+	route := server.NewRoute(nil, "/test")
+	Expect(route.route).ShouldNot(BeNil())
+	err := route.AddMethod(http.MethodPost, func(request HttpRequest) (statusCode int, response *JSendMessage) {
+		if string(request.Body) == "success" {
+			return http.StatusOK, NewJSendMessage(http.StatusOK)
+		}
+		return http.StatusNotAcceptable, NewJSendMessage(http.StatusNotAcceptable)
+	})
+	Expect(err).ShouldNot(HaveOccurred())
+	closeServer, _ := server.RunServer()
+	defer closeServer()
+	resp, err := http.Post(baseUrl+"/test", "application/json", bytes.NewReader([]byte("success")))
+	Expect(err).ShouldNot(HaveOccurred())
+	Expect(resp.Status).Should(BeEquivalentTo(strconv.Itoa(http.StatusOK) + " OK"))
 }
